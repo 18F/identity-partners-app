8c64e01b6cdb43009bd81791a325c5154a01f786
Oren Kanner <oren.kanner@gsa.gov>
2020-10-28 16:24:51 -0400

    Add model specs for Account

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,7 @@
 class Account < ApplicationRecord
   has_many :users
   has_many :apps
-  validates :lg_account_id, presence: true, uniqueness: true
+  validates :lg_account_id, presence: true,
+                            uniqueness: { case_sensitive: false }
   validates :name, presence: true
 end

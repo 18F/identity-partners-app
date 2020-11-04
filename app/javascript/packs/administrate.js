caa38c3f280f2ddbff0c11afd5d3a964575eaeb4
Oren Kanner <oren.kanner@gsa.gov>
2020-11-04 18:30:20 -0500

    Fix datepicker / turbolinks

--- app/javascript/packs/administrate.js
+++ app/javascript/packs/administrate.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-// require("turbolinks").start()
+require("turbolinks").start()
 
 // The next line you only need if you want ActiveStorage support
 require("@rails/activestorage").start()

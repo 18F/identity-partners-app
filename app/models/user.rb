366dfafffe297706dddbe436e5e39d97361ef8e9
Oren Kanner <oren.kanner@gsa.gov>
2020-11-13 16:03:05 -0500

    Clean up partner-facing views for demo (#18)
    Remove a bunch of unused non-Administrate code (controllers/views, etc)
    and clean up the accounts#show view to actually work (query optimization
    to come).

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   devise :trackable, :timeoutable
 
+  has_many :account_contacts, dependent: :destroy
+  has_many :accounts, through: :account_contacts
+  has_many :integration_contacts, dependent: :destroy
+  has_many :integrations, through: :integration_contacts
+
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :uuid, uniqueness: { case_sensitive: false }, allow_nil: true
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 Rails.application.routes.draw do
   namespace :admin do
-      resources :accounts
-      resources :iaa_gtcs
-      resources :iaa_orders
-      resources :integrations
-      resources :users
-      resources :account_contacts, only: %i[index show new create destroy]
-      resources :integration_contacts, only: %i[index show new create destroy]
-      resources :agencies
-      resources :account_statuses
-      resources :iaa_statuses
-      resources :integration_statuses
+    resources :accounts
+    resources :iaa_gtcs
+    resources :iaa_orders
+    resources :integrations
+    resources :users
+    resources :account_contacts, only: %i[index show new create destroy]
+    resources :integration_contacts, only: %i[index show new create destroy]
+    resources :agencies
+    resources :account_statuses
+    resources :iaa_statuses
+    resources :integration_statuses
 
-      root to: "users#index"
-    end
-  devise_for :users
+    root to: "accounts#index"
+  end
+
   devise_for :users, skip: [:sessions]
 
   devise_scope :user do
@@ -24,12 +24,8 @@ Rails.application.routes.draw do
   end
 
   get '/auth/logindotgov/callback' => 'users/omniauth#callback'
-  get 'users/none' => 'users#none'
 
-  resources :users
-
-  get '/accounts/all' => 'accounts#all'
-  resources :accounts
+  resources :accounts, only: [:index, :show]
 
   root to: 'home#index'
 end

477ceb655ff21324d0fd7cbe79bd1e5044ad0aea
Oren Kanner <oren.kanner@gsa.gov>
2020-11-13 00:01:24 -0500

    Clean up partner-facing views for demo
    Remove a bunch of unused non-Administrate code (controllers/views, etc)
    and clean up the accounts#show view to actually work (query optimization
    to come).

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,13 +2,10 @@ class HomeController < ApplicationController
   def index
     render :index and return unless user_signed_in?
 
-    # @account = Account.find_by(lg_account_id: current_user.lg_account_id)
-
-    if @account
-      render 'home/authenticated/index'
+    if current_user.accounts.count.positive?
+      redirect_to accounts_path
     else
       render 'home/authenticated/no_account'
     end
-
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,13 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
     it { is_expected.to validate_uniqueness_of(:uuid).case_insensitive.allow_nil }
   end
+
+  describe 'associations' do
+    subject { build(:user) }
+
+    it { is_expected.to have_many(:account_contacts) }
+    it { is_expected.to have_many(:accounts).through(:account_contacts) }
+    it { is_expected.to have_many(:integration_contacts) }
+    it { is_expected.to have_many(:integrations).through(:integration_contacts) }
+  end
 end
